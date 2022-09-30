7be3529880ea221c3735a62e70f628c86116d274
İsmail BOYACI <76762856+ismailboyaci@users.noreply.github.com>
2022-09-30 16:31:06 +0300

    update servie index

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 
-var username=sessionStorage.getItem("email");
-var password=sessionStorage.getItem("password");
+/* var username=sessionStorage.getItem("email");
+var password=sessionStorage.getItem("password"); */
 export default axios.create({
   baseURL: "https://test001.testnet.mobiliz.com.tr/interview",
   headers: {
